7188d478465682fd020d7e743ab3b615bd5858c2
pavi2410 <hello@pavi2410.me>
2023-10-10 15:06:46 +0530

    push

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 use std::io::{Read, Write};
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
@@ -9,17 +10,23 @@ fn main() {
             Ok(mut stream) => {
                 println!("accepted new connection");
 
-                let mut buf = [0; 1024];
-
-                loop {
-                    stream.read(&mut buf).unwrap();
-                
-                    stream.write(b"+PONG\r\n").unwrap();
-                }
+                thread::spawn(|| {
+                    handle_connection(stream);
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+
+    loop {
+        stream.read(&mut buf).unwrap();
+
+        stream.write(b"+PONG\r\n").unwrap();
     }
 }
