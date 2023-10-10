01dcd46964b14bf2b43ce4c0253bd152a6c4b83b
pavi2410 <hello@pavi2410.me>
2023-10-10 14:52:21 +0530

    push

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                
+                stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
